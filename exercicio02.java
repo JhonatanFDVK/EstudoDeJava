912dd43313fe0812d1f3f74ac1d38e97510da8ba
Jhonatan Silva <78217344+JhonatanFDVK@users.noreply.github.com>
2022-10-06 09:30:38 -0300

    Add files via upload

---
+++ exercicio02.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class exercicio02 {
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        int valor = input.nextInt();
+        int antecessor = valor - 1;
+
+        System.out.println("Antecessor de " + valor + " : " + antecessor);
+
+        input.close();
+        
+    }
+}
