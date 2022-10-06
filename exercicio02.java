a015b5334b81036a531a0d15e14f7313c64e75bd
Jhonatan Silva <78217344+JhonatanFDVK@users.noreply.github.com>
2022-10-06 09:31:16 -0300

    Update exercicio02.java

--- exercicio02.java
+++ exercicio02.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class exercicio02 {
+public class Exercicio02 {
     public static void main(String[] args) {
         
         Scanner input = new Scanner(System.in);
